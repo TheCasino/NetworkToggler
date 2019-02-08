74ae8e1c6e1eed593e313f58970c7fc6b0343bca
Casino Boyale <kayrenhi@gmail.com>
2019-02-08 15:20:22 +0000

    Initial Commit
    :D

---
+++ NetworkToggler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Management;
+
+namespace NetworkToggler
+{
+    internal class Program
+    {
+        private const string ClassName = "Win32_NetworkAdapter";
+        private const string PropName = "NetEnabled";
+        private const string DisableMethodName = "Disable";
+        private const string EnableMethodName = "Enable";
+
+        private static void Main()
+        {
+            var management = new ManagementClass(ClassName);
+            var instances = management.GetInstances();
+            
+            foreach (var baseInstance in instances)
+            {
+                if(!(baseInstance is ManagementObject instance))
+                    continue;
+                
+                var netEnabled = instance[PropName];
+
+                if (!(netEnabled is bool enabled) || !enabled)
+                    continue;
+
+                instance.InvokeMethod(DisableMethodName, null);
+                instance.InvokeMethod(EnableMethodName, null);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-NetworkToggler
+So if you're like me and you're stuck in 2009 and still play Modern Warfare 2 and go through the agony of dealing with a closed NAT type and have to painstakingly deal with disabling and re-enabling your network adapter to open your NAT type then fear no more! This app will disable all of your enabled network adapters then enable them again! 
